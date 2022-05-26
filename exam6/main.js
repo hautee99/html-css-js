f40f94f4fe12514a014163d97de9b780c1966034
Xuan Hau <haute6868@gmail.com>
2022-05-26 14:34:10 +0700

    update code 2

--- exam6/main.js
+++ exam6/main.js
@@ -1,2 +1,8 @@
-// Regular exxpression kiem tra so dien thoai dau 84 hoac +84 or 03
+// Regular exxpression kiem tra so dien thoai dau 84 or +84 or 03
 const regex = /(((\+|)84)|0)(3)+([0-9]{8})\b/;
+// [ab] khop a or b
+//(ab || cd) nhom ki tu chua mot khoi
+// + xuat hien mot den nhieu lan
+// {n} xuat hien 8 lan
+
+// bắt đầu bằng +84 or 84 or 03 rồi lấy ngẫu nhiên 0 - 9 xuất hiện 8 lần
